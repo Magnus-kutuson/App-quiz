22821a0b1025fc223c94de6c63d91f5fce7aac4b
Magnus-kutuson <magnus.kutuson@amalitech.com>
2024-11-28 17:10:28 +0000

    done with the home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service'; 
+import { Data } from '../data'; 
+import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  standalone: true,  
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrls: ['./home-page.component.css'],
+  imports: [CommonModule]  
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  allQuizzes: Data[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+  
+    this.allQuizzes = this.dataService.getData();
+    console.log(this.allQuizzes);  
+  }
 }
